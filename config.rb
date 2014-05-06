08878b0a51df76ec778219b31ea1234da3ea03c8
Steve Holmes <iamsteveholmes@gmail.com>
2014-05-05 21:07:21 -0700

    Updated site

---
+++ config.rb
@@ -0,0 +1,20 @@
+
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/"
+http_images_path = "/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/fonts"
+css_path = "/Users/steve/projects/coder-consortium/.cache/compass/stylesheets"
+
+# Local development paths
+sass_path = "/Users/steve/projects/coder-consortium/theme/sass"
+generated_images_path = "/Users/steve/projects/coder-consortium/.cache/compass/images"
+images_path = "/Users/steve/projects/coder-consortium/theme/images"
+fonts_path = "/Users/steve/projects/coder-consortium/theme/fonts"
+
+line_comments = false
+output_style = :compressed
