023474fd3f15d543fd38512faaf2e292d775625c
Steve Holmes <steven.holmes@ucdmc.ucd.edu>
2014-07-03 11:45:15 -0700

    Updated site

--- config.rb
+++ config.rb
@@ -8,13 +8,13 @@ http_path = "/"
 http_images_path = "/images"
 http_generated_images_path = "/images"
 http_fonts_path = "/fonts"
-css_path = "/Users/steve/projects/coder-consortium/.cache/compass/stylesheets"
+css_path = "C:/Users/sholmes2.HS/projects/coderconsortium.github.io/.cache/compass/stylesheets"
 
 # Local development paths
-sass_path = "/Users/steve/projects/coder-consortium/theme/sass"
-generated_images_path = "/Users/steve/projects/coder-consortium/.cache/compass/images"
-images_path = "/Users/steve/projects/coder-consortium/theme/images"
-fonts_path = "/Users/steve/projects/coder-consortium/theme/fonts"
+sass_path = "C:/Users/sholmes2.HS/projects/coderconsortium.github.io/theme/sass"
+generated_images_path = "C:/Users/sholmes2.HS/projects/coderconsortium.github.io/.cache/compass/images"
+images_path = "C:/Users/sholmes2.HS/projects/coderconsortium.github.io/theme/images"
+fonts_path = "C:/Users/sholmes2.HS/projects/coderconsortium.github.io/theme/fonts"
 
 line_comments = false
 output_style = :compressed
